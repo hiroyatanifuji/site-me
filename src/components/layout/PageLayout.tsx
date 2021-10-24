15c8b01ca36cc4410f9ce7e1a381f8967ab35efa
hiroya tanifuji <xxhiroyy18@gmail.com>
2021-10-24 13:16:03 +0900

    feat: implement PageLayout component

---
+++ src/components/layout/PageLayout.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Head from 'next/head';
+import styled from 'styled-components';
+import ColorConstant from '../../constants/color';
+import RequiredChildrenComponent from '../../types/RequiredChildrenComponent';
+
+const StyledDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: ${ColorConstant.PRIMARY_COLOR};
+`;
+
+const StyledSelection = styled.section`
+  max-width: 375px;
+  margin: 0 auto;
+`;
+
+const PageLayout: RequiredChildrenComponent<{ title: string }> = ({ title, children }) => (
+  <StyledDiv>
+    <Head>
+      <title>{title}</title>
+    </Head>
+    <StyledSelection>{children}</StyledSelection>
+  </StyledDiv>
+);
+
+export default PageLayout;
