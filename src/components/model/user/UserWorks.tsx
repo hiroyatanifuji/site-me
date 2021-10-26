5a8c3d0567c86e7fbf403654366189ad561df4ad
hiroya tanifuji <xxhiroyy18@gmail.com>
2021-10-26 19:33:30 +0900

    feat: implement UserWorks component

---
+++ src/components/model/user/UserWorks.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+import SubTitle from '../../ui/text/SubTitle';
+import PostListTile from '../../ui/tile/PostListTile';
+
+export { LinkType } from '../../ui/tile/PostListTile';
+
+const StyledSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 100px;
+`;
+
+const PostList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 30px;
+`;
+
+const StyledPostListTile = styled(PostListTile)`
+  margin-right: 10px;
+  margin-bottom: 10px;
+`;
+
+const UserWorks: React.FunctionComponent<{ data: (React.ComponentProps<typeof PostListTile> & { id: number })[] }> = ({
+  data,
+}) => (
+  <StyledSection>
+    <SubTitle title="Recent Works" />
+    <PostList>
+      {data.map(({ id, type, href, thumbnailUrl, title }) => (
+        <StyledPostListTile key={id} type={type} href={href} thumbnailUrl={thumbnailUrl} title={title} />
+      ))}
+    </PostList>
+  </StyledSection>
+);
+
+export default UserWorks;
