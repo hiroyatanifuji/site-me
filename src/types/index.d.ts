8d6baa8442c5f7fd08c8c185dea832f36697e603
hiroya tanifuji <xxhiroyy18@gmail.com>
2021-10-26 20:27:39 +0900

    feat: uninstall better-typescript-lib

--- src/types/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="../../node_modules/better-typescript-lib/lib.dom.generated.d.ts" />
-/// <reference path="../../node_modules/better-typescript-lib/lib.dom.iterable.d.ts" />
-/// <reference path="../../node_modules/better-typescript-lib/lib.dom.iterable.generated.d.ts" />
-/// <reference path="../../node_modules/better-typescript-lib/lib.esnext.d.ts" />
